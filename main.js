b3d601902bad23ea02e6f8606bae8179239775b7
A-even-cooler-guy <157364874+A-even-cooler-guy@users.noreply.github.com>
2024-04-17 12:19:40 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera")
+function take_snapshot() {
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='captured_image' src="+data_uri+">";
+})
+}
+
+
+console.log("ml5 version", ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/aBbz05sEB/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model has succsesfully loaded.")
+}
+
+function check(){
+    image=document.getElementById("captured_image");
+    classifier.classify(image,gotResult);
+}
+
+function gotResult(error,results){
+    var synth=window.speechSynthesis;
+if(error){
+    console.error(error);
+   
+}
+else{
+    console.log(results);
+    document.getElementById("object").innerHTML=results[0].label; 
+}
+
+}
